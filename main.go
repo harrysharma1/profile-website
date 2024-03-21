e075684d32e81b81aab9c49fb3390e96bd2e173d
Harry Sharma <harrysharma1066@gmail.com>
2024-03-21 15:46:06 +0000

    feat: create basic file routes for pages

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
 
+
 func main()  {
-	
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("template/index.html"))		
+		tmpl.Execute(w, nil)
+	})	
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("template/about.html"))
+		tmpl.Execute(w,nil)
+	})
+	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("template/contact.html"))
+		tmpl.Execute(w,nil)
+	})
 
 	log.Fatal(http.ListenAndServe(":6969", nil))
 }
