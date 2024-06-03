21713c06cf8b3cbbd2e6486dfe626df8b0e80516
Harry Sharma <harrysharma1066@gmail.com>
2024-06-03 13:55:52 +0100

    feat: switch to glob to use partials

--- main.go
+++ main.go
@@ -4,23 +4,34 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
 )
 
 
 
 func main()  {
+	tmpl, err := template.ParseGlob("templates/*")  
+	if err != nil{
+		log.Fatal(err)	
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("template/index.html"))		
-		tmpl.Execute(w, nil)
+		err = tmpl.ExecuteTemplate(w,"index",nil)
+		if err != nil { 
+			http.Error(w,err.Error(),http.StatusInternalServerError)
+		}
 	})	
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("template/about.html"))
-		tmpl.Execute(w,nil)
+		err = tmpl.ExecuteTemplate(w,"about",nil)
+		if err != nil {
+			http.Error(w,err.Error(),http.StatusInternalServerError)
+		}
 	})
 	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("template/contact.html"))
-		tmpl.Execute(w,nil)
+		err = tmpl.ExecuteTemplate(w,"contact",nil)
+		if err != nil {
+			http.Error(w,err.Error(),http.StatusInternalServerError)
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":6969", nil))
