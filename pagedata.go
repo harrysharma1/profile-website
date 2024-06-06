259c9f9883f3e2e70ebe49796f8a7a504961049a
Harry Sharma <harrysharma1066@gmail.com>
2024-06-06 11:31:57 +0100

    update: finish more of the timeline

--- pagedata.go
+++ pagedata.go
@@ -143,8 +143,8 @@ var about_data = AboutData{
 			<p> It is split into two sections and a 20 Hours Project: </p>
 			<br>
 			<ol>
-				<li><strong>Computer systems</strong> My favourite topics were: Wired and wireless networks, Network topologies, protocols and layers, System security, System software</li>
-				<li><strong>Computational thinking, algorithms and programming</strong> My favourite topics were: Data representation, Algorithms, Programming techniques</li>
+				<li><strong>Computer systems:</strong> My favourite topics were: Wired and wireless networks, Network topologies, protocols and layers, System security, System software</li>
+				<li><strong>Computational thinking, algorithms and programming:</strong> My favourite topics were: Data representation, Algorithms, Programming techniques</li>
 				<li><strong>Programming Project:</strong> My project consisted of a phsysics simulaton, written in Java, that mapped projectile motion. Sadly I had not used any version control system and the computer I programmed it in is bricked so this has been lost to time.
 			</ol>
 			`,
@@ -156,26 +156,59 @@ var about_data = AboutData{
 			<p> I completed my A-levels in Covid so  I won't go too much into detail but my grades were:</p>
 			<br>
 			<ol>
-			<li>Maths: A </li>
-			<li>Computer Science: B</li>
-			<li>Chemisry B </li> 
+			<li><strong>Maths:</strong> A </li>
+			<li><strong>Computer Science:</strong> B</li>
+			<li><strong>Chemistry:</strong> B </li> 
+			</ol>
 			`,
 		},
 		{
 			Date: "September 2021",
 			Title: "Start University in King's College London",
 			Text: `
+			<p>Started my first year of university and the first year out of thw Pandemic. </p>
+			<p>Adjusting to the workload was difficult at first and I even failed a module or two. </p>
+			<p>This became an eye opener for me as I had become complacent with my studies. </p>
+			<p>The modules in first year were all mandatory. They consisted of:</p>
+			<br>
+			<ul>
+				<li><strong>Elementary Logic with Applications</strong></li>
+				<li><strong>Introduction to Software Engineering</strong></li>
+				<li><strong>Database Systems</strong></li>
+				<li><strong>Data Structures</strong></li>
+				<li><strong>Foundations of Computing 1</strong></li>
+				<li><strong>Computer Systems</strong></li>
+				<li><strong>Programming Practice & Applications</strong></li>
+				<li><strong>Introduction to Professional Practice</strong></li>
+			</ul>
 			`,
 		},
 		{
 			Date: "May 2023",
 			Title: "Finish 2nd Year Exams",
-			Text: `Hello`,
+			Text: `
+			<p> I came into second year with a renewed vigor. </p>
+			<p> The modules I selected were </p>
+			<br>
+			<ul>
+				<li><strong>Software Engineering Group Project</strong></li>
+				<li><strong>Operating Systems & Concurrency</strong></li>
+				<li><strong>Foundations of Computing 2</strong></li>
+				<li><strong>Introduction to Artificial Intelligence</strong></li>
+				<li><strong>Practical Experiences of Programming</strong></li>
+				<li><strong>Internet Systems</strong></li>
+				<li><strong>Programming Language Design Paradigms</strong></li>
+			</ul>
+			<br>
+			<p>Along with the modules I needed to apply for placement positions.</p>
+			<p>After countless rejection in various stages of the application phase, I managed to secure two offers.</p>
+			<p>I took the offer to work with e-Research, which is a sub-department of the Research Management and Innovation Division (RMID) within King's College London.</p>
+			`,
 		},
 		{
 			Date: "September 2023",
 			Title: "Start Industry Placement in e-Research",
-			Text: `Hello`,
+			Text: `<p><PLACEHOLDER NOT FINISHED YET</p>`,
 		},
 	},	
 }
