433664617a627f1d64323b8f46ee374fc305eb5d
Harry Sharma <harrysharma1066@gmail.com>
2024-12-22 00:22:28 +0000

    feat: add goodreads update widget

--- pagedata.go
+++ pagedata.go
@@ -32,10 +32,11 @@ type GoodReadsBox struct {
 }
 
 type IndexData struct {
-	Welcome IndexWelcomeBox
-	Flake8  IndexFlake8Box
-	Spack   IndexSpackBox
-	GoShell GoShellBox
+	Welcome   IndexWelcomeBox
+	Flake8    IndexFlake8Box
+	Spack     IndexSpackBox
+	GoShell   GoShellBox
+	GoodReads GoodReadsBox
 }
 
 var index_data IndexData = IndexData{
@@ -114,37 +115,41 @@ class="hover:underline hover:text-gray-300">King's College London</a>, where I a
 		Style: []template.CSS{
 			`		
 			#customize-list{
-				float:left;
-				margin-left:20px;
-				list-style:none;
-			}`,
+			float:left;
+			margin-left:20px;
+			list-style:none;
+			}
+			`,
 			`
 			#gr_updates_widget{
 				float:left;
 				border-radius: 5px;
 				background-color:#fff;
-				border:solid #683205 10px;
-				-webkit-box-shadow: 0px 0px 4px 1px #595959,
-				inset 0px 0px 0px 1px #7D730B;
+				border:solid #82e822 10px;
+				-webkit-box-shadow: 0px 0px 4px 1px #70fa07,
+				inset 0px 0px 0px 1px #eedb0d;
 				-moz-box-shadow: 0px 0px 4px 1px #595959,
 				inset 0px 0px 0px 1px #7D730B;
 				box-shadow: 0px 0px 4px 1px #595959,
 				inset 0px 0px 0px 1px #7D730B;
 				padding:15px 0 0px 15px;
-				width:250px;
-				height:565px;
-	  		}`,
+				width:400px;
+				height:365px;
+			}
+			`,
 			`
 			#gr_footer{
 				margin-bottom:0px;
 				height:30px;
-			}`,
+			}
+			`,
 			`
 			#gr_updates_widget p{
 				padding:0px;
 				margin:0;
 				font-size:14px;
-			}`,
+			}
+			`,
 			`
 			#gr_footer img{
 				width:100px;
@@ -155,10 +160,10 @@ class="hover:underline hover:text-gray-300">King's College London</a>, where I a
 		Content: []template.HTML{
 			`
 			<div id="gr_updates_widget">
-			<iframe sandbox id="the_iframe" src="https://goodreads.com/widgets/user_update_widget?height=600&num_updates=10&user=184799979&width=250" width="248" height="530" frameborder="0"></iframe>
-			<div id="gr_footer">
-				<a href="https://www.goodreads.com/"><img alt="Goodreads: Book reviews, recommendations, and discussion" src="https://s.gr-assets.com/images/layout/goodreads_logo_140.png" /></a>
-			</div>
+				<iframe sandbox id="the_iframe" src="https://goodreads.com/widgets/user_update_widget?height=250&num_updates=20&user=184799979&width=400" width="398" height="180" frameborder="0"></iframe>
+					<div id="gr_footer">
+						<a href="https://www.goodreads.com/"><img alt="Goodreads: Book reviews, recommendations, and discussion" src="https://s.gr-assets.com/images/layout/goodreads_logo_140.png" /></a>
+					</div>
 			</div>
 			`,
 		},
